79aeed7c9da63c63fea204f59a1a191cc8e277d8
ammymeena <40714565+ammymeena@users.noreply.github.com>
2018-06-30 17:56:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'clc5go7GkujiSMA1Yo4iwYRni8P8AvGTpQSl+15mVdTM0irg6BBFJr9l0BTKYStOFS6KtODelavjWJ7m0vOTsnsnifKh1oDCOhFsZdiAN8xg0vx/3BhnwBKPwQmfqgsOx2QDcfUF38D3IwaZjxbIUQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
