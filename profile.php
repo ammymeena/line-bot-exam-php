b33230f73dffd2ebcf8369702331046340758076
ammymeena <40714565+ammymeena@users.noreply.github.com>
2018-06-30 17:59:35 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'clc5go7GkujiSMA1Yo4iwYRni8P8AvGTpQSl+15mVdTM0irg6BBFJr9l0BTKYStOFS6KtODelavjWJ7m0vOTsnsnifKh1oDCOhFsZdiAN8xg0vx/3BhnwBKPwQmfqgsOx2QDcfUF38D3IwaZjxbIUQdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Uaa339944dcb4d6b946dfc750aa9ce9ea';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
