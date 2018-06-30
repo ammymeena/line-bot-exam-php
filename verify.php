7f082abca75a381c02c148aaa890cb218c57f82f
ammymeena <40714565+ammymeena@users.noreply.github.com>
2018-06-30 17:58:50 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'clc5go7GkujiSMA1Yo4iwYRni8P8AvGTpQSl+15mVdTM0irg6BBFJr9l0BTKYStOFS6KtODelavjWJ7m0vOTsnsnifKh1oDCOhFsZdiAN8xg0vx/3BhnwBKPwQmfqgsOx2QDcfUF38D3IwaZjxbIUQdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
